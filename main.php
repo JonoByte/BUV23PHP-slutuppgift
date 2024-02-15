d2e624f34d01ccd39cb3401461f0c03e5663a03a
Ninpoz <92263460+Ninpoz@users.noreply.github.com>
2024-02-15 11:33:56 +0100

    Fixade CSS mapstruktur samt flyttade på home och browse

--- main.php
+++ main.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-    <link rel="stylesheet" href="mainstyle.css">
+    <link rel="stylesheet" href="CSS/main.css">
 
     <!-- Bootstrap CSS v5.2.1 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
@@ -21,34 +21,19 @@
             <div class=header-text>
             <h1>Gamescore</h1>
             <h2>Play, Review, Connect Your Gaming Community Awaits!</h2>
-            <h3>Browse</h3>
+            <h3>Home</h3>
         </div>
         </header>
         <div class="nav">
-            <a href="main.php">Browse</a>
-            <a href="#">Forum</a>
-            <a href="#">Friends</a>
-            <a href="#">Login</a>
+            <a href="main.php">Home</a>
+            <a href="browse.php">Browse</a>
+            <a href="forum.php">Forum</a>
+            <a href="friends.php">Friends</a>
+            <a href="login.php">Login</a>
         </div>
 
         <div id="main">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <?php for($i=0; $i<20; $i++): ?>
-                    <div class="col-lg-3 col-md-3 col-sm-6">
-                        <div class="card mb-4">
-                            <img class="card-img-top" src="placeholder.jpg" alt="Card image">
-                            <div class="card-body">
-                                <h5 class="card-title">Game Title</h5>
-                                <p class="card-text">lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>
-                                <a href="#" class="btn btn-primary">test test</a>
-                            </div>
-                        </div>
-                    </div>
-                    <?php endfor; ?>
-                </div>
-            </div>
-        </div>
+
         </div>
 
     </div>
