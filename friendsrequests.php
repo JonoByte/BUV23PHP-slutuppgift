5d645519a85bb7ebef2069ca7d210d64dcb7aa87
Alvekvist <52975495+Alvekvist@users.noreply.github.com>
2024-02-15 16:01:41 +0100

    Friends site added

--- friendsrequests.php
+++ friendsrequests.php
@@ -32,55 +32,18 @@
             <a href="login.php">Login</a>
         </div>
 
-        <div class="main">
-            <div class="container">
-                <img class="profile" src="img/profile.jpg" alt="profile picture">
-                <h1>Username</h1>
-            </div>
-
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Add Friend</button>
-
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="exampleModalLabel">Add Friend</h1>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <form>
-                                <div class="mb-3">
-                                    <label for="recipient-name" class="col-form-label">Friends Username:</label>
-                                    <input type="text" class="form-control" id="recipient-name">
-                                </div>
-                                <div class="mb-3">
-                                    <label for="message-text" class="col-form-label">Message:</label>
-                                    <textarea class="form-control" id="message-text"></textarea>
-                                </div>
-                            </form>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Send message</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <a href="friendsrequests.php"><button type="button" class="btn btn-primary">Friends Requests</button></a>
-        </div>
 
         <div id="friend">
             <div class="container">
                 <div class="row justify-content-center">
-                    <?php for ($i = 0; $i < 20; $i++) : ?>
+                    <?php for ($i = 0; $i < 6; $i++) : ?>
                         <div class="col-lg-3 col-md-3 col-sm-6">
                             <div class="card mb-4">
                                 <img class="card-img-top" src="img/profile.jpg" alt="Card image">
                                 <div class="card-body">
                                     <h5 class="card-title">Username</h5>
                                     <p class="card-text">lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>
-                                    <a href="#" class="btn btn-primary">Send message</a>
+                                    <a href="#" class="btn btn-primary">Accept Friends Request</a>
                                 </div>
                             </div>
                         </div>
@@ -88,6 +51,7 @@
                 </div>
             </div>
         </div>
+
 
     </div>
 
