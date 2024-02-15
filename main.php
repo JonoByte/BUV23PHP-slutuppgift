a03287f958936830faa58d44450f758eae402874
Ninpoz <92263460+Ninpoz@users.noreply.github.com>
2024-02-15 16:43:21 +0100

    Fixat homepage

--- main.php
+++ main.php
@@ -19,10 +19,10 @@
         <header>
             <!-- <img src="img/header-pic.jpg" alt="lololol"> -->
             <div class=header-text>
-            <h1>Gamescore</h1>
-            <h2>Play, Review, Connect Your Gaming Community Awaits!</h2>
-            <h3>Home</h3>
-        </div>
+                <h1>Gamescore</h1>
+                <h2>Play, Review, Connect Your Gaming Community Awaits!</h2>
+                <h3>Home</h3>
+            </div>
         </header>
         <div class="nav">
             <a href="main.php">Home</a>
@@ -33,10 +33,119 @@
         </div>
 
         <div id="main">
+            <div class="main-header">
 
-        </div>
+                <!-----Carousel function------>
 
-    </div>
+                <div id="carouselExampleDark" class="carousel carousel-light slide">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active" data-bs-interval="5000">
+                            <img src="img/wow.jpg" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h1>We have the latest news in the gaming world</h1>
+                                <h2>search your favourite game and learn all from tips and tricks to lore</h2>
+                                <h3>make friends and join the community</h3>
+                                
+                            </div>
+                        </div>
+                        <div class="carousel-item" data-bs-interval="2000">
+                            <img src="img/overwatch.jpg" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                            <h1>We have the latest news in the gaming world</h1>
+                                <h2>search your favourite game and learn all from tips and tricks to lore</h2>
+                                <h3>make friends and join the community</h3>
+                                
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="img/pubg.jpg" class="d-block w-100" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                            <h1>We have the latest news in the gaming world</h1>
+                                <h2>search your favourite game and learn all from tips and tricks to lore</h2>
+                                <h3>make friends and join the community</h3>
+                              
+                            </div>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+                <!-----Carousel function END------>
+
+            
+
+            </div>
+
+
+            <div class="background-image">
+
+
+                <div class="container">
+                    <div class="row justify-content-end">
+
+                        <!--Card 1 Start--->
+                        <div class="col-lg-3 col-md-3 col-sm-6">
+                            <div class="card mb-4">
+                                <img class="card-img-top" src="img/1.jpg" alt="Card image">
+                                <div class="card-body">
+                                    <h5 class="card-title">Game Title</h5>
+                                    <p class="card-text">lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>
+                                    <a href="#" class="btn btn-primary">test test</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!--Card 2 Start--->
+                        <div class="col-lg-3 col-md-3 col-sm-6">
+                            <div class="card mb-4">
+                                <img class="card-img-top" src="img/kisspng-halo-4-halo-5-guardians-halo-the-master-chief-co-glowing-halo-5abe2f35099df7.4138996515224133650394.png" alt="Card image">
+                                <div class="card-body">
+                                    <h5 class="card-title">Game Title</h5>
+                                    <p class="card-text">lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>
+                                    <a href="#" class="btn btn-primary">test test</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!--Card 3 Start--->
+
+                        <div class="col-lg-3 col-md-3 col-sm-6">
+                            <div class="card mb-4">
+                                <img class="card-img-top" src="img/10.jpg" alt="Card image">
+                                <div class="card-body">
+                                    <h5 class="card-title">Game Title</h5>
+                                    <p class="card-text">lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>
+                                    <a href="#" class="btn btn-primary">test test</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!--Card 4 Start--->
+                        <div class="col-lg-3 col-md-3 col-sm-6">
+                            <div class="card mb-4">
+                                <img class="card-img-top" src="img/3.jpg" alt="Card image">
+                                <div class="card-body">
+                                    <h5 class="card-title">Game Title</h5>
+                                    <p class="card-text">lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>
+                                    <a href="#" class="btn btn-primary">test test</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
 
 
 
@@ -48,10 +157,12 @@
 
 
 
-    <!-- Bootstrap JavaScript Libraries -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+
+            <!-- Bootstrap JavaScript Libraries -->
+            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
 </body>
 
 </html>
